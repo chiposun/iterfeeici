c848ad8f7a2a034eaf5058cf850f44499b5714ec
pc1_25 <pc1_25@DESKTOP-I3126SM>
2024-01-09 15:27:24 +0200

    Add project files.

---
+++ iterfeeici/IDrawable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iterfeeici
+{
+    public interface IDrawable
+    {
+        void Draw();
+
+        
+    }
+
+}
+
+    
+
+    
+    
+       
+    
+
